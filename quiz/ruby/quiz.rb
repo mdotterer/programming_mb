8e79fe2db65d0aa50f4d7c0544ac79261929afcd
Michael Dotterer <mdotterer@gmail.com>
2026-02-06 15:29:41 -0500

    Clean up the choice handling to make it more accessible

--- quiz/ruby/quiz.rb
+++ quiz/ruby/quiz.rb
@@ -7,13 +7,13 @@ questions = JSON.load(File.open("../questions.json"))
 questions.each.with_index(1) do |question, i|
   puts "Question ##{i}\n"
   puts question['text']
-  question['choices'].each.with_index('A'.ord) do |choice, letter|
-    puts "#{letter.chr}) #{choice}"
+  question['choices'].each do |(letter, choice)|
+    puts "#{letter}) #{choice}"
   end
   puts "\n"
   print "Your Answer: "
 
-  answer = gets.strip.upcase.ord - 'A'.ord
+  answer = gets.strip.upcase
 
   if answer == question['answer']
     puts "Correct!"
